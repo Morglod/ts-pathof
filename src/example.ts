4e01d949ced8c560e9b8f5f80d6bca714a107d48
Ambroise Rabier <rabier.ambroise@outlook.fr>
2019-08-27 19:10:08 +0200

    Updated example with an array case.

--- src/example.ts
+++ src/example.ts
@@ -3,7 +3,9 @@ import { pathOf, pathOf3 } from ".";
 const a = { x: 10 };
 const b = { x: a };
 const c = { z: { y: { bb: 123 }}};
+const d = {x: [{b: 123}]};
 
 const r1 = pathOf(a, 'x');
 const r2 = pathOf(b, 'x');
 const r3: pathOf3<typeof c, 'z', 'y', 'bb'> = pathOf(c, 'z', 'y', 'bb');
+const r4 = pathOf(d, 'x', 0, 'b');
